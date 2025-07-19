4f7924f2e9642a6f8b2410f22824779c703f2195
Gloomwing <ai454293996@outlook.com>
2025-07-19 10:51:39 +0800

    add dotenv and solidity-coverage to hardhat configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("./tasks/block-number");
+require("dotenv").config();
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
